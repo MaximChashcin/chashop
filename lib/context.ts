b3c51058454c6ea7889379ff617667db51f0d263
Chashcin Maxim <chashcinmaxim@gmail.com>
2023-07-13 23:22:38 +0300

    Delete context.ts

--- lib/context.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext({ user: null, chashcin: false })
-export const WalletContext = createContext({ current: 0, total: 0 })
